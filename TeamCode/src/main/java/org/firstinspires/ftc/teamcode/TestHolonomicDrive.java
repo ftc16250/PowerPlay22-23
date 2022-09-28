58db78f1df86a2e478dd988f78784c3922378c8b
Mespyr <senukcw@gmail.com>
2022-09-28 09:22:29 -0400

    added function which will make it easier to change power and direction of all motors at once

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHolonomicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHolonomicDrive.java
@@ -10,23 +10,17 @@ public class TestHolonomicDrive extends HolonomicDrive {
     public void loop() {
         // test if game-pads work
         if (gamepad1.dpad_up) {
-            // set direction
-            frontLeftWheel.setDirection(DcMotorSimple.Direction.FORWARD);
-            frontRightWheel.setDirection(DcMotorSimple.Direction.FORWARD);
-            backLeftWheel.setDirection(DcMotorSimple.Direction.FORWARD);
-            backRightWheel.setDirection(DcMotorSimple.Direction.FORWARD);
-            // set power so they spin
-            frontLeftWheel.setPower(1);
-            frontRightWheel.setPower(1);
-            backLeftWheel.setPower(1);
-            backRightWheel.setPower(1);
+            setMotorDirection(
+                    DcMotorSimple.Direction.FORWARD,
+                    DcMotorSimple.Direction.FORWARD,
+                    DcMotorSimple.Direction.FORWARD,
+                    DcMotorSimple.Direction.FORWARD
+            );
+            setMotorPower(1, 1, 1, 1);
         }
         else {
             // don't spin motor if nothing is pressed
-            frontLeftWheel.setPower(0);
-            frontRightWheel.setPower(0);
-            backLeftWheel.setPower(0);
-            backRightWheel.setPower(0);
+            setMotorPower(0, 0, 0, 0);
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16250/HolonomicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16250/HolonomicDrive.java
@@ -2,23 +2,40 @@ package org.firstinspires.ftc.teamcode.ftc16250;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class HolonomicDrive extends OpMode {
-    public DcMotor frontLeftWheel;
-    public DcMotor frontRightWheel;
-    public DcMotor backLeftWheel;
-    public DcMotor backRightWheel;
+    public DcMotor frontLeftMotor;
+    public DcMotor frontRightMotor;
+    public DcMotor backLeftMotor;
+    public DcMotor backRightMotor;
 
     @Override
     public void init()
     {
         // get wheel motors from robot hardware config
-        frontLeftWheel = hardwareMap.dcMotor.get("frontLeftWheel");
-        frontLeftWheel = hardwareMap.dcMotor.get("frontRightWheel");
-        backLeftWheel = hardwareMap.dcMotor.get("backLeftWheel");
-        backLeftWheel = hardwareMap.dcMotor.get("backLeftWheel");
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        frontLeftMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
     }
 
     @Override
     public void loop() {}
+
+    // set the direction of all motors
+    public void setMotorDirection(DcMotorSimple.Direction flDirection, DcMotorSimple.Direction frDirection, DcMotorSimple.Direction blDirection, DcMotorSimple.Direction brDirection) {
+        frontLeftMotor.setDirection(flDirection);
+        frontRightMotor.setDirection(frDirection);
+        backLeftMotor.setDirection(blDirection);
+        backRightMotor.setDirection(brDirection);
+    }
+
+    // set power of all motors
+    public void setMotorPower(int flPower, int frPower, int blPower, int brPower) {
+        frontLeftMotor.setPower(flPower);
+        frontRightMotor.setPower(frPower);
+        backLeftMotor.setPower(blPower);
+        backRightMotor.setPower(brPower);
+    }
 }
